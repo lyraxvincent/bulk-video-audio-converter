7f70715c16ea14146e0bbde91fca80036db9ec4c
lyraxvincent <njongevincent@gmail.com>
2020-03-17 22:39:37 +0300

    Initial commit

---
+++ conveter.py
@@ -0,0 +1,18 @@
+import os
+from moviepy.editor import *
+
+dirpath = str(input("Enter the target directory path(include '/' at the end): "))
+pathlist = [vid for vid in os.listdir(dirpath)]
+
+audiopath_prompt = str(input("Do you wish to use the same directory for the audiofiles? [Y/n]: "))
+if audiopath_prompt == 'Y'.lower():
+    audiopath = dirpath
+elif audiopath_prompt == 'N'.lower():
+    audiopath = str(input("Enter destination folder for the audio files('/' at the end): "))
+else:
+    print("Invalid entry.")
+
+for i in pathlist:
+    vclip = VideoFileClip(os.path.join(dirpath, pathlist[0]))
+    audioclip = vclip.audio
+    audioclip.write_audiofile('{}.mp3'.format(os.path.join(audiopath, i)))
