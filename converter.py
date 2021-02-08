4f078a8fbec40c7e9b66cf2900fd9a1a5b133719
Jans Johnson <jaansjohnson@gmail.com>
2021-02-09 00:20:32 +0530

    Added an option to select files with specific extensions

--- converter.py
+++ converter.py
@@ -1,18 +1,27 @@
 import os
 from moviepy.editor import *
 
-dirpath = str(input("Enter the target directory path(include '/' at the end): "))
+dirpath = str(input("Enter the target directory path: "))
 pathlist = [vid for vid in os.listdir(dirpath)]
+fileFormats=["mp4","mov","wmv","avi","mkv"]             #Enter your audio formats here
+files=[]
+for i in pathlist:
+    try:
+        if(i.split(".")[1] in fileFormats):
+            files.append(i)
+    except:
+        continue
+print(files)
 
 audiopath_prompt = str(input("Do you wish to use the same directory for the audiofiles? [Y/n]: "))
-if audiopath_prompt == 'Y'.lower():
+if audiopath_prompt == 'Y' or audiopath_prompt == 'y':
     audiopath = dirpath
-elif audiopath_prompt == 'N'.lower():
-    audiopath = str(input("Enter destination folder for the audio files('/' at the end): "))
+elif audiopath_prompt == 'N' or audiopath_prompt == 'n':
+    audiopath = str(input("Enter destination folder for the audio files: "))
 else:
     print("Invalid entry.")
 
-for vid in pathlist:
+for vid in files:
     vclip = VideoFileClip(os.path.join(dirpath, vid))
     audioclip = vclip.audio
-    audioclip.write_audiofile('{}.mp3'.format(os.path.join(audiopath, vid.split('.mp4')[0]))) # Removing video extension to replace it with audio extension
+    audioclip.write_audiofile('{}.mp3'.format(os.path.join(audiopath, vid.split('.')[0]))) # Removing video extension to replace it with audio extension
